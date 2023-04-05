ea687b257bd9b02aca81aaef5fbed6b97546b94c
sooraj vijayan <sooraj@hamon.in>
2023-04-05 10:20:07 +0530

    User list button moved to Form component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Form from "./components/Form";
 import List from "./components/List";
 import Error from "./components/Error";
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -11,11 +10,6 @@ function App() {
       style={{ backgroundColor: "#E1F8DC", height: "100vh" }}
       className="App"
     >
-      <div>
-        <button className="listButton">
-          <NavLink to="/list">View all users</NavLink>
-        </button>
-      </div>
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/list" element={<List />} />
