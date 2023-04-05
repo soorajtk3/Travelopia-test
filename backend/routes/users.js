3380370184d1d8696ecfff066e46c149a398c82d
sooraj vijayan <sooraj@hamon.in>
2023-04-05 17:20:22 +0530

    Swagger documentation wrotes for GET and POST API calls

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,6 +5,7 @@ var router = express.Router();
 const prisma = new PrismaClient();
 
 /* GET users listing. */
+
 router.get("/", async function (req, res) {
   let getUser = await prisma.user.findMany();
   if (!getUser) {
@@ -12,6 +13,17 @@ router.get("/", async function (req, res) {
   }
   return res.status(200).json({ getUser });
 });
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get all users
+ *     description: Retrieve a list of all users.
+ *     responses:
+ *       200:
+ *         description: A list of users
+ */
+
 router.post("/", async function (req, res) {
   let { name, email, destination, travellers, budget } = req.body;
   let userDetails = await prisma.user.create({
@@ -27,5 +39,43 @@ router.post("/", async function (req, res) {
     userDetails,
   });
 });
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         destination:
+ *           type: string
+ *         travellers:
+ *           type: integer
+ *         budget:
+ *           type: integer
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create travel booking by a user
+ *     description: Create a booking
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: Create booking success
+ */
 
 module.exports = router;
