8e4110949f7b7ca364fc7929180c72356ae4ebdd
sooraj vijayan <sooraj@hamon.in>
2023-04-06 10:47:04 +0530

    Updated error page style properly and given option for redirect to home

--- frontend/src/components/Error.js
+++ frontend/src/components/Error.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
-  <div>
-    <h1>404 Error</h1>
-    <h1>Page Not Found</h1>
-  </div>;
+  console.log("error");
+  return (
+    <div>
+      <div>
+        <h1 style={{ color: "red" }}>404 Error</h1>
+        <h1>Page Not Found</h1>
+      </div>
+      <div>
+        <button className="listButton">
+          <NavLink to="/">Back to Home</NavLink>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
